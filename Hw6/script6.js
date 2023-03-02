01b4d1c7989d97fa2d2101a7c573890892e1d568
PavRazgulin <121128137+PavRazgulin@users.noreply.github.com>
2023-03-02 22:30:52 +0300

    Add files via upload

---
+++ Hw6/script6.js
@@ -0,0 +1,28 @@
+const task1 = document.getElementById(".super_link");
+console.log(task1);
+
+const task2 = document.querySelector(".card-link");
+task2.textContent = "ссылка";
+
+
+const task3 = document.getElementsByClassName("card-link,card-body");
+console.log(task3);
+
+const task4 = document.getElementById("data-number");
+if (".data-number" == "50") {
+  console.log(".data-number");
+}
+
+const task5 = document.getElementsByTagName("title");
+console.log(task5);
+
+const task6 = document.getElementsByClassName("card-title");
+console.log(task6.parentNode);
+
+const task7 = document.querySelector(".card");
+const p = document.createElement("p");
+p.textContent = "Привет";
+card.appendChild("p");
+
+  const task8 = document.getElementById(".h6");
+  task8 remove();
